86a8ead869eca5e27f3df39a34ef2756d5073c95
arfana123 <80836910+arfana123@users.noreply.github.com>
2021-03-17 08:15:31 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+ $("#animate").click(function(){
+
+        $("#square").animate({
+            width: '+=20px'
+        },500);
+
+        $("#square").animate({
+            height: '+=20px'
+        },5000);
+
+    });
